c48b0bbb4505aeca7de1280c79b01eff2e699341
Geoff Thornburrow <geofftnz@gmail.com>
2017-09-16 13:50:57 +1200

    setting up project

---
+++ ParticleViewer/Program.cs
@@ -0,0 +1,48 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ParticleViewer
+{
+    static class Program
+    {
+        private static Logger log = LogManager.GetCurrentClassLogger();
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        [PermissionSet(SecurityAction.Demand, Name = "FullTrust")]
+        static void Main()
+        {
+            log.Info("Particles START");
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Startup());
+
+            try
+            {
+                using (var v = new Snowscape.TerrainGenerationViewer.TerrainGenerationViewer())
+                {
+                    v.Run(60);
+                }
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show(ex.Message, ex.GetType().Name);
+
+                log.Error(ex, "uncaught exception");
+
+                throw;
+            }
+
+
+            log.Info("Particles END");
+        }
+    }
+}
+
