b851d8bf14d8b641f47f56855f2941b2884cbaa3
Geoff Thornburrow <geofftnz@gmail.com>
2017-09-19 20:46:51 +1200

    tweaks

--- Particulate/Resources/Shaders/particles.glsl
+++ Particulate/Resources/Shaders/particles.glsl
@@ -15,21 +15,17 @@ void main()
 	//pos = gl_Position.xyz;
 	//texcoord = vertex.xy;
 
-	//vec4 s = vec4(32.0,0.0,0.0,1.0);
-	//s = inverse(projectionMatrix) * s;
-	//s = inverse(viewMatrix) * s;
-	//s = inverse(modelMatrix) * s;
-	//s /= s.w;
-
 	// https://stackoverflow.com/questions/8608844/resizing-point-sprites-based-on-distance-from-the-camera
-	float s = 1.0;
+	float s = 2.0;
 
-	vec4 pos = vec4(vertex.x,vertex.y,mod(vertex.x*vertex.y*50.0,1.0),1.0);
-	vec4 eyePos = viewMatrix * modelMatrix * pos;
+	vec4 p = vec4(vertex.x,vertex.y,mod(vertex.x*vertex.y*50.0,1.0),1.0);  //TODO read from texture.
+
+	vec4 eyePos = viewMatrix * modelMatrix * p;
 	vec4 corner = projectionMatrix * vec4(s,s,eyePos.z,eyePos.w);
 	size = corner.x / corner.w;
 	gl_Position = projectionMatrix * eyePos;
 	gl_PointSize = size;
+	pos = p.xyz;
 }
 
 //|frag
@@ -42,11 +38,13 @@ layout (location = 0) out vec4 out_Colour;
 
 void main(void)
 {
-	vec3 col = vec3(pos * 0.05 + 0.5);
+	vec3 col = vec3(0.1) + vec3(pos);
 
 	vec2 pc = (gl_PointCoord.st - vec2(0.5)) * 2.0;
 	float rsq = dot(pc,pc);
+
 	float a = (1.0 - smoothstep(0.5,1.0,rsq));
+	a *= (1.0 / (1.0 + rsq * 50.0));
 
 	a *= min(1.0,size*0.1);
 	
